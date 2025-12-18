c18186bd97daa5228b54611893f516bede7e72df
Carolina Garrido T. <ca.garridot@duocuc.cl>
2025-12-17 23:54:33 -0300

    apk creada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.ecomarketmovil"
         minSdk = 24
         targetSdk = 36 // Actualizado para coincidir con compileSdk
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
